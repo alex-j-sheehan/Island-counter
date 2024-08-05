0d6d89d1f64e36dddc305e6759e52d5e48ee60fa
HilbertsSpace <morgan.r.bronson.18@gmail.com>
2024-08-05 17:55:18 -0400

    added montecarlo simulations for 1D expected island count and tested against theoretically derived excpected count function dependant on width and water sample frequency

---
+++ island_count_monte_carlo.py
@@ -0,0 +1,37 @@
+from islands import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+WIDTH = 100
+HEIGHT = 1
+STEPSIZE = .02
+SIMULATIONS = 400
+
+
+binned_samps = []
+bins = []
+
+fstep = .01
+while fstep < 1:
+    samps = []
+    for i in range(SIMULATIONS):
+        ocean = create_ocean(length=HEIGHT, width=WIDTH, frequency=fstep)
+        #print_ocean(ocean)
+        Icount = count_distinct_islands(ocean)
+        samps.append(Icount)
+    bins.append(fstep)
+    binned_samps.append(np.mean(samps))
+    fstep+=STEPSIZE
+
+
+plt.plot(bins, binned_samps, alpha=.8, color='blue')
+plt.plot(bins, [island_count_expectation_valie_2d(WIDTH, b) for b in bins], alpha=.5, color='red')
+plt.title('Width: %d'%WIDTH)
+plt.legend(['Simulation', 'Expected Value'])
+plt.show()
+plt.clf()
+
+
+
+
